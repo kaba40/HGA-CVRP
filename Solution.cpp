bb1c85e5dcb6b29660a506222ca585102ed18dcf
Kaba Keita <kaba.keita@inria.fr>
2018-11-28 09:08:12 +0100

    création Solution.cpp et solution.hpp 28/11/18

---
+++ Solution.cpp
@@ -0,0 +1,18 @@
+/*
+ * Solution.cpp
+ *
+ *  Created on: 27 nov. 2018
+ *      Author: kkeita
+ */
+
+#include "Solution.hpp"
+
+Solution::Solution() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Solution::~Solution() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Solution.hpp
@@ -0,0 +1,17 @@
+/*
+ * Solution.hpp
+ *
+ *  Created on: 27 nov. 2018
+ *      Author: kkeita
+ */
+
+#ifndef SOLUTION_HPP_
+#define SOLUTION_HPP_
+
+class Solution {
+public:
+	Solution();
+	virtual ~Solution();
+};
+
+#endif /* SOLUTION_HPP_ */
